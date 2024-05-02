078194315ab834f15b3757eec1d723739cdf48b0
Keanin Cupido <keanin16@gmail.com>
2024-05-02 07:02:39 +0200

    setup tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 
 function App() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        {/* Header */}
+        <nav>
+          navbar
+        </nav>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    )
 }
 
 export default App;
